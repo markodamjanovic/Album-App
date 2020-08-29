8a95bd70ad0f066eb55eb9f950f24f72057cd93b
markodamjanovic <damjanovic.m1@gmail.com>
2020-08-29 11:20:16 +0200

    initial startup mvc app

---
+++ AlbumApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlbumApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
