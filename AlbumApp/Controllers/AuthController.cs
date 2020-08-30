f3fc30b57bc735db069a9602ffddc5633c2c9c10
markodamjanovic <damjanovic.m1@gmail.com>
2020-08-30 14:18:24 +0200

    AuthController changes for ApplicationUser

--- AlbumApp/Controllers/AuthController.cs
+++ AlbumApp/Controllers/AuthController.cs
@@ -1,19 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using AlbumApp.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
+using AlbumApp.Models;
 
 namespace AlbumApp.Controllers
 {
     public class AuthController : Controller
     {   
-        private readonly UserManager<IdentityUser> userManager;
-        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
 
-        public AuthController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        public AuthController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
@@ -28,18 +26,24 @@ namespace AlbumApp.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel model)
         {
-            if (ModelState.IsValid){
-                var user = new IdentityUser {UserName = model.Email, Email = model.Email};
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser {FirstName = model.FirstName, 
+                                                LastName = model.LastName, 
+                                                UserName = model.Email, 
+                                                Email = model.Email};
+
                 var result = await userManager.CreateAsync(user, model.Password);
 
-                if(result.Succeeded){
+                if(result.Succeeded)
+                {
                     await signInManager.SignInAsync(user, isPersistent:false);
                     return RedirectToAction("index", "home");
                 }
                 
                 foreach(var error in result.Errors)
                 {
-                    ModelState.AddModelError("", error.Description);
+                    ModelState.AddModelError(string.Empty, error.Description);
                 }
 
             }
@@ -51,9 +55,36 @@ namespace AlbumApp.Controllers
             return View();
         }
 
-        public IActionResult LogOut(){
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl)
+        {
 
-            return View();
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+
+                if(result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(returnUrl))
+                    {
+                        return RedirectToAction("index", "home");
+                    }
+                    else
+                    {
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+                
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LogOut()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("index", "home");
         }
     }
 }
