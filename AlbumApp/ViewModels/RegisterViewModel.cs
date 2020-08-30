2e8c16e9445a00a1568f635e8b83ba52f61c861b
markodamjanovic <damjanovic.m1@gmail.com>
2020-08-30 10:28:28 +0200

    register view model

---
+++ AlbumApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+
+namespace AlbumApp.ViewModels{
+
+    public class RegisterViewModel{
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
