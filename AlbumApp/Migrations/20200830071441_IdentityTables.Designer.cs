5a46e437a983b7f7ae52327a410200fafa779a54
markodamjanovic <damjanovic.m1@gmail.com>
2020-08-30 09:28:17 +0200

    identity migration and .db file

--- AlbumApp/Migrations/20200830071441_IdentityTables.Designer.cs
+++ AlbumApp/Migrations/20200830071441_IdentityTables.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using AlbumApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AlbumApp.Migrations
 {
     [DbContext(typeof(AlbumContext))]
-    partial class AlbumContextModelSnapshot : ModelSnapshot
+    [Migration("20200830071441_IdentityTables")]
+    partial class IdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
