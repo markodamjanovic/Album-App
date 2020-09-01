2659c1862c5ca8b07cd1a2e4d0bc35eb15d51db6
markodamjanovic <damjanovic.m1@gmail.com>
2020-09-02 00:34:05 +0200

    helper classes for deserializing json

---
+++ AlbumApp/Utility/Caption.cs
@@ -0,0 +1,8 @@
+namespace AlbumApp.Utility
+{
+    public class Caption
+    {
+        public string Text { get; set; }
+        public float Confidence { get; set; }
+    }
+}
---
+++ AlbumApp/Utility/Description.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+
+namespace AlbumApp.Utility
+{
+    public class Description
+    {
+        public List<string> Tags { get; set; }
+        public List<Caption> Captions { get; set; }
+    }
+}
