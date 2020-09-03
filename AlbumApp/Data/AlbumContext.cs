becd13335ce60f6021afcfeb8169c59776b0c1de
markodamjanovic <damjanovic.m1@gmail.com>
2020-09-03 11:14:17 +0200

    unused dependency

--- AlbumApp/Data/AlbumContext.cs
+++ AlbumApp/Data/AlbumContext.cs
@@ -2,13 +2,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore; 
 
 using AlbumApp.Models;
-
+using System;
 namespace AlbumApp.Data
 {
     public class AlbumContext : IdentityDbContext<ApplicationUser>
-    {
+    {   
          public AlbumContext(DbContextOptions<AlbumContext> options): base(options)
-         {}
+         {
+         }
 
          public DbSet<Photo> Album {get; set;}
     }
